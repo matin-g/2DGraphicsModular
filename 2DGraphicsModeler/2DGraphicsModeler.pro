aac9deff1e1d18e64bea1d8d9b6b64f14397b7c0
oliveryue2-Arthur Siu <51488740+oliveryue2@users.noreply.github.com>
2019-07-03 00:16:41 -0700

    Added vector class
    Bugged for now. Writing into wrong memory when using iterators in insert() and erase(). Will push new code when finished debugging and light testing.

--- 2DGraphicsModeler/2DGraphicsModeler.pro
+++ 2DGraphicsModeler/2DGraphicsModeler.pro
@@ -29,7 +29,8 @@ SOURCES += \
         mainwindow.cpp
 
 HEADERS += \
-        mainwindow.h
+        mainwindow.h \
+        vector.h
 
 FORMS += \
         mainwindow.ui
---
+++ 2DGraphicsModeler/vector.h
@@ -0,0 +1,206 @@
+#pragma once
+#ifndef VECTOR_H
+#define VECTOR_H
+#include <exception>
+template<typename T>
+class vector
+{
+public:
+	vector() :size_v{ 1 }, elem{ new T }, space{ 1 }
+	{}// end of vector
+
+	explicit vector(int s) : size_v{ s }, space{ s }
+	{
+		elem = new T[s];
+	}// end of vector(int)
+
+
+	vector(const vector& rhs) : space{ rhs.space }, size_v{ rhs.size_v }
+	{
+		elem = new T[rhs.space];
+		for (int i = 0; i < rhs.size_v; i++)
+		{
+			*(elem + i) = *(rhs.elem + i);
+		}
+	}//end of vector(const vector&)
+
+	vector& operator=(const vector& rhs) // copy assignment
+	{
+		if (this != &rhs)
+		{
+			if (size_v <= rhs.size_v) //if vector is less that rhs
+			{
+				delete[] elem;
+				elem = new T[rhs.size_v];
+				size_v = rhs.size_v;
+				space = rhs.space;
+			}
+			else
+			{
+				size_v = rhs.size_v;
+			}
+			for (int i = 0; i < size_v; i++)
+				*(elem + i) = *(rhs.elem + i);
+		}
+
+	}
+	vector(vector&& rhs) // move constructor
+		:size_v {rhs.size_v}, space {rhs.space}
+	{
+		elem = rhs.elem;
+		rhs.elem = nullptr;
+		rhs.size = 0;
+		rhs.space = 0;
+	}
+	vector& operator=(vector&& rhs) // move assignment
+	{
+		if (this != &rhs)
+		{
+			delete[] elem;
+			elem = rhs.elem;
+			rhs.elem = nullptr;
+			rhs.size = 0;
+			rhs.space = 0;
+		}
+		return *this;
+	}
+	~vector() // destructor
+	{
+		delete[] elem;
+		elem = nullptr;
+	}
+	const T& operator[] (int index) // access: return reference
+	{
+		if (index >= size_v)
+		{
+			throw std::out_of_range("This is out of range of the vector. Invalid access.");
+		}
+		return elem[index];
+	}
+	int size() const // the current size
+	{
+		return size_v;
+	}
+	int capacity() const // current available space
+	{
+		return space;
+	}
+	void resize(int newsize) // grow
+	{
+
+		T* temp = new T[resize];
+		int i = 0;
+		while (i < size_v)
+		{
+			for (i; i < resize; i++)
+			{
+				temp[i] = elem[i];
+			}
+		}// this is not like the real STL vector where we can accept another
+		//default argument to initialize data if our old vector is smaller that the resize.
+	}
+	void push_back(T val) // add element
+	{
+		if (size_v < space)
+		{
+			elem[++size_v] = val;
+		}
+		else
+		{
+			reserve(2 * space);
+			elem[++size_v] = val;
+		}
+	}
+	void reserve(int newalloc) // get more space
+	{
+	
+		T* temp = new T[newalloc];
+		for (int i = 0; i < size_v; i++)
+		{
+			temp[i] = elem[i];
+		}
+		delete[] elem;
+		elem = temp;
+		temp = nullptr;
+		space = newalloc;
+	}
+	using iterator = T * ;
+	using const_iterator = const T*;
+	iterator begin() // points to first element
+	{
+		return elem;
+	}
+	const_iterator begin() const
+	{
+		return elem;
+	}
+	iterator end() // points to one beyond the last element
+	{
+		return elem + space;
+	}
+	const_iterator end() const
+	{
+		return elem + space;
+	}
+	iterator insert(iterator p, const T& v)// insert a new element v before p
+	{
+		T* temp = nullptr;
+		if (size_v >= space)
+		{
+			temp = new T[size_v*2];
+			space = size_v * 2;
+		}
+		else
+		{
+			temp = new T[space];
+		}
+		int i = 0;
+		iterator j;
+		for (j = begin(); j < p; j++)
+		{
+			temp[i] = *j;
+			i++;
+		}
+		iterator returnIt = temp+i;
+		temp[i] = v;
+		i++;
+
+		for (j; j != end(); j++)
+		{
+			temp[i] = *j;
+			i++;
+		}
+
+		size_v++;
+		delete[] elem;
+		elem = temp;
+		temp = nullptr;
+		
+		return returnIt;
+	}
+	iterator erase(iterator p) // remove element pointed to by p
+	{
+		T* temp = new T[space];
+
+		int i = 0;
+		for (i; i < p; ++i)
+		{
+			temp[i] = elem[i];
+		}
+		++i;
+		for (i; i < size_v; ++i)
+		{
+			temp[i-1] = elem[i];
+		}
+
+		delete[] elem;
+		elem = temp;
+		temp = nullptr;
+	}
+private:
+	int size_v;
+	T* elem;
+	int space;
+};
+
+#endif // VECTOR_H
