4a9dc4fd69a324f656cbeb01b866669a9d58609e
oliveryue2-Arthur Siu <51488740+oliveryue2@users.noreply.github.com>
2019-07-05 16:15:02 -0700

    Function for testing vector class
    self-explanatory

---
+++ 2DGraphicsModeler/vector_test.cpp
@@ -0,0 +1,52 @@
+#include "vector_test.h"
+void test()
+{
+	std::cout << "Starting vector test:\n";
+	std::cout << "Creating new vector:\n";
+	vector<int> *test_vector_int = new vector<int>(3);
+
+	std::cout << "Populating new vector with  +33 elements:\n";
+	for (int i = 0; i < 33; i++)
+		test_vector_int->push_back(i);
+
+	std::cout << "Resize to 4\n";
+	test_vector_int->resize(4);
+
+	std::cout << "Reserve to 400\n";
+	test_vector_int->reserve(400);
+
+	std::cout << "The capacity of the test_vector_int is: " << \
+		test_vector_int->capacity() << std::endl;
+	std::cout << "The size of the test_vector_int is: " << \
+		test_vector_int->capacity() << std::endl;
+	std::cout << "The capacity of the test_vector_int is: " << \
+		test_vector_int->size() << std::endl;
+	std::cout << "Elements in test_vector: ";
+	for (auto p = test_vector_int->begin(); p != test_vector_int->end; p++)
+		std::cout << *p << ' ';
+	std::cout << std::endl;
+
+	std::cout << "Inserting 43 at begin()+2\n";
+	test_vector_int->insert((test_vector_int->begin)+2, 43);
+	std::cout << "Erasing at begin()+1\n";
+	test_vector_int->erase((test_vector_int->begin) + 1);
+
+	std::cout << "Calling copy constructor\n";
+	vector<int> *test_vector_int2 = new vector<int>(*test_vector_int);
+
+	std::cout << "pushback() test_vector_int2 with  +33 elements:\n";
+	for (int i = 0; i < 33; i++)
+		test_vector_int2->push_back(i);
+
+	std::cout << "Calling copy assignment test = test2\n";
+	*test_vector_int = *test_vector_int2;
+
+	std::cout << "Delete 1st test vector\n";
+	delete test_vector_int;
+	test_vector_int = nullptr;
+	std::cout << "Delete 2nd test vector\n";
+	delete test_vector_int;
+	test_vector_int2 = nullptr;
+
+	system("pause");
+}
