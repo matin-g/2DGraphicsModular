446377c30f8ced9c5cc914d986da09102e12c769
oliveryue2-Arthur Siu <51488740+oliveryue2@users.noreply.github.com>
2019-07-05 11:46:15 -0700

    Shape Update
    Puts some comments and followed Kath's suggestions. I'm learning the qt draw tutorail as I don't know enough about QPaint to implement the correct inheritance heirarchy

--- 2DGraphicsModeler/shape.cpp
+++ 2DGraphicsModeler/shape.cpp
@@ -1,11 +1,5 @@
 #include "shape.h"
 
-Shape::Shape():shapeId {0}, shapeType {"Empty"}, shapeDimensions {0},\
-
-    penColor {Qt::black}, penWidth {0}, penStyle { Qt::NoPen},\
-    penCapStyle {Qt::FlatCap}, penJoinStyle {Qt::MiterJoin},\
-
-    textString {"Empty"}, textColor{Qt::black}, textAlignment {Qt::AlignLeft},\
-    textPointSize {-1}, textFontFamily {"Comic Sans MS"},\
-    textFontStyle {QFont::StyleNormal} , textFontWeight {QFont::Thin}
+Shape::Shape(): shapeId {0},\
+    shape {NONE}, pen(),brush()
 {}
--- 2DGraphicsModeler/shape.h
+++ 2DGraphicsModeler/shape.h
@@ -3,8 +3,11 @@
 #include <string>
 #include <Qt>
 #include <QFont>
+#include <QPen>
+#include <QBrush>
 #include "vector.h"
 
+enum ShapeType{NONE,LINE,POLYLINE, POLYGON,RECTANGLE,SQUARE, ELLIPSE};
 class Shape
 {
 public:
@@ -13,24 +16,25 @@ public:
     const QBrush& get_brush()const;
 
     void set_shape(ShapeType shape);
-    void set_pen(Qt::GlobalColor, int width, Qt:PenStyle, Qt::PPenCapStyle, Qt::PenJointStyle);
-    void set_brush(Qt::GlobalColor, Qt::BrushStyle)
+    void set_pen(Qt::GlobalColor, int width, Qt::PenStyle, Qt::PenCapStyle, Qt::PenJoinStyle);
+    void set_brush(Qt::GlobalColor, Qt::BrushStyle);
 
     void default_style();
-    void draw_rect(int width, int height);
-
-    virtual void draw(const int x, const int y);
+    void draw_rect(int width,   int height);
+    virtual void draw(const int x, const int y)=0;
 protected:
     QPainter& get_qpainter();
 
 private:
     QPainter& qpainter;
 
+    //shapeDimensions must be inherited
+
     int shapeId;
     ShapeType shape;
     QPen pen;
     QBrush brush;
-
+/* Temporary
     std::string shapeType;
     vector<vector<int>> shapeDimensions;
 
@@ -49,7 +53,7 @@ private:
     int textPointSize;
     std::string textFontFamily;
     QFont::Style textFontStyle;
-    QFont::Weight textFontWeight;
+    QFont::Weight textFontWeight;*/
 };
 
 #endif // SHAPE_H
