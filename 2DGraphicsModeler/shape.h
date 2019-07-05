25255794f71ba2b721aa781ec772b005934bf600
oliveryue2 <51488740+oliveryue2@users.noreply.github.com>
2019-07-05 10:42:54 -0700

    Update shape.h
    Just some notes on Shape. Will change in an hour or 2.

--- 2DGraphicsModeler/shape.h
+++ 2DGraphicsModeler/shape.h
@@ -9,8 +9,28 @@ class Shape
 {
 public:
     Shape();
+    virtual ~Shape();
+    const QBrush& get_brush()const;
+
+    void set_shape(ShapeType shape);
+    void set_pen(Qt::GlobalColor, int width, Qt:PenStyle, Qt::PPenCapStyle, Qt::PenJointStyle);
+    void set_brush(Qt::GlobalColor, Qt::BrushStyle)
+
+    void default_style();
+    void draw_rect(int width, int height);
+
+    virtual void draw(const int x, const int y);
+protected:
+    QPainter& get_qpainter();
+
 private:
+    QPainter& qpainter;
+
     int shapeId;
+    ShapeType shape;
+    QPen pen;
+    QBrush brush;
+
     std::string shapeType;
     vector<vector<int>> shapeDimensions;
 
